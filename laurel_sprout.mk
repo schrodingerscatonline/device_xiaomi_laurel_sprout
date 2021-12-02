4022e01e855b39e7aac04b292b41dae734e8c3f6
anirudhkosgi <kosgi34@gmail.com>
2021-12-02 19:32:35 +0000

    laurel_sprout: remove Xiaomiparts

--- laurel_sprout.mk
+++ laurel_sprout.mk
@@ -277,7 +277,6 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.sh \
     init.recovery.qcom.rc \
     init.target.rc \
-    init.xiaomi_parts.rc \
     ueventd.qcom.rc
 
 # Keymaster
@@ -554,10 +553,6 @@ PRODUCT_PACKAGES += \
     libaacwrapper \
     libnl \
     libwfdaac_vendor
-
-# Xiaomi
-PRODUCT_PACKAGES += \
-    XiaomiParts
 
 # Inherit the proprietary files
 $(call inherit-product-if-exists, vendor/xiaomi/laurel_sprout/laurel_sprout-vendor.mk)
